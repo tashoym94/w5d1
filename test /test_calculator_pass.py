5b0c73bdd9002b4cf7f6625e4a2cfc49d73e3dc8
tashoym94 <tashoy.miller@gmail.com>
2025-10-27 20:04:29 -0400

    Update test_calculator_pass.py

--- test /test_calculator_pass.py
+++ test /test_calculator_pass.py
@@ -1,1 +1,8 @@
+from app.calculator import add, safe_divide
+import math
 
+def test_add_simple():
+    assert add(2, 2) == 4
+
+def test_safe_divide_regular():
+    assert math.isclose(safe_divide(9, 3), 3.0)
