be443e3105b269b658353db0ec1dba37d7225cb8
tashoym94 <tashoy.miller@gmail.com>
2025-10-27 20:04:07 -0400

    Update test_calculator_fail.py

--- test /test_calculator_fail.py
+++ test /test_calculator_fail.py
@@ -1,1 +1,10 @@
+from app.calculator import add, safe_divide
+import pytest
 
+def test_add_incorrect_on_purpose():
+    # Fixed assertion: add(2, 3) should equal 5.
+    assert add(2, 3) == 5
+
+def test_safe_divide_zero_error():
+    with pytest.raises(ZeroDivisionError):
+        safe_divide(1, 0)
