ef1d682e993dc1f0e199a3d5d5d4ad63dd89813e
tashoym94 <tashoy.miller@gmail.com>
2025-10-27 20:05:40 -0400

    Update calculator.py

--- app/calculator.py
+++ app/calculator.py
@@ -1,1 +1,7 @@
+def add(a: float, b: float) -> float:
+    return a + b
 
+def safe_divide(a: float, b: float) -> float:
+    if b == 0:
+        raise ZeroDivisionError("Division by zero is not allowed.")
+    return a / b
